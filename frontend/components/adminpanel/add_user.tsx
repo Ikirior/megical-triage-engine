4545bbff4d0ed23277badad713f306b6c368d07d
guirque <guirque@gmail.com>
2026-02-19 23:41:50 -0300

    refactor: changing user management strategy
    Code was refactored to make use of the form tag instead of the more programmatic approach. This improves flexibility in selecting column order and changing aspects of individual input fields, as well as highly simplifies the code.

--- frontend/components/adminpanel/add_user.tsx
+++ frontend/components/adminpanel/add_user.tsx
@@ -1,19 +1,24 @@
 'use server';
 
-import { randomUUID } from "crypto";
-import cell from "./cell";
+export default async function AddUser(params: FormData) {
 
-type addUserParams = {
-    id: string,
-    fields: cell
-}
+    const payload = {
+        "name": params.get('name'),
+        "email": params.get('email'),
+        "cpf": params.get('cpf'),
+        "rg": params.get('rg'),
+        "role": params.get('role'),
+        "specialization": params.get('specialization'),
+        "password": "string"
+    }
 
-export default async function AddUser(params: addUserParams) {
-    const addReq = await fetch(`http://backend_server:3001/users/${params.id}`, {
+    const addReq = await fetch(`http://backend_server:3001/users/${params.get('id')}`, {
         "method": "POST",
-        "body": JSON.stringify({...params.fields, password: randomUUID.call(params.fields)})
+        "body": JSON.stringify(payload)
     })
     let res = addReq.status;
-    console.log(res, {...params.fields, password: randomUUID.call(params.fields)});
+
+    console.log('added');
+
     return res
 }
--- frontend/components/adminpanel/update_user.tsx
+++ frontend/components/adminpanel/update_user.tsx
@@ -1,17 +1,24 @@
 'use server';
 
-import cell from "./cell";
+export default async function UpdateUser(params: FormData) {
 
-type UpdateUserParams = {
-    id: string
-    fields: cell
-}
+    const payload = {
+        "name": params.get('name'),
+        "email": params.get('email'),
+        "cpf": params.get('cpf'),
+        "rg": params.get('rg'),
+        "role": params.get('role'),
+        "specialization": params.get('specialization'),
+        "password": "string"
+    }
 
-export default async function UpdateUser(params: UpdateUserParams) {
-    let uptReq = await fetch(`http://backend_server:3001/users/${params.id}`, {
+    let uptReq = await fetch(`http://backend_server:3001/users/${params.get('id')}`, {
         "method": "PUT",
-        "body": JSON.stringify(params.fields)
+        "body": JSON.stringify(payload)
     })
     let res = uptReq.status;
+
+    console.log('updated')
+
     return res
 }
