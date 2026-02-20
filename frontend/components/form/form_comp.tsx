5048357f226a34af1970810cd8a31db59140eba9
guirque <guirque@gmail.com>
2026-02-20 16:40:54 -0300

    feat: adding basic login

--- frontend/components/form/form_comp.tsx
+++ frontend/components/form/form_comp.tsx
@@ -1,16 +1,17 @@
 import styles from "@/components/form/form_comp.module.css"
 import Link from "next/link"
+import Login from "./login"
 
 export default function FormComp()
 {
     return (
-        <form className={styles.login_form} method="POST">
+        <form className={styles.login_form} method="POST" action={Login}>
 
             <h1 style={{"fontSize": "1.6em", "margin": "auto"}}>Login</h1>
 
             <div className={styles.input_group}>
                 <label htmlFor="id">E-mail or Username</label>
-                <input type="email" name="id" id="id" />
+                <input type="text" name="id" id="id" />
             </div>
 
             <div className={styles.input_group}>
@@ -21,7 +22,6 @@ export default function FormComp()
                     Forgot my password
                 </Link>
             </div>
-
 
             <input type="submit"></input>
         </form>
