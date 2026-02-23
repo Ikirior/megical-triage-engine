9069edaa51cc123fe6a7b4862d6d21b9ef5c55fe
guirque <guirque@gmail.com>
2026-02-23 00:18:39 -0300

    feat: adding step text loading

--- frontend/components/triagesheet/stepsection.tsx
+++ frontend/components/triagesheet/stepsection.tsx
@@ -7,20 +7,42 @@ type stepsectionparams = {
     currentStep: null|1|2|3
 }
 
+const CONTENT = {
+    0:{
+        "description": <>Start by choosing a triage from the <b>queue</b>. The <b>queue</b> contains both the information of patients who are waiting for their triage to start, but also of those whose triage has started, but was left unfinished.</>,
+        "next_step": <>During step 1, you'll fill in the <b>Essentials</b> (vitals) topic and add <b>Observations</b>.</>
+    },
+    1: {
+        "description": <>Fill in the<b>Essentials</b> and <b>Observations</b> fields.</>,
+        "next_step": <>Durin step 2, you'll receive question suggestions made by MedGemma.</>
+    },
+    2: {
+        "description": "",
+        "next_step": ""
+    },
+    3: {
+        "description": "",
+        "next_step": ""
+    }
+}
+
 export default function StepSection(params: stepsectionparams)
 {   
+
+    const step_num = params.currentStep as keyof typeof CONTENT ?? 0;
+
     return <SidePanel position="right">
         <div className={styles.step}>
-            <div className={styles.step}>1</div>
+            <div className={styles.step}>{step_num}</div>
             <div className={styles.blueline}></div>
         </div>
-        <h1>Step {params.currentStep}</h1>
+        <h1>Step {step_num}</h1>
         <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quae nihil voluptatem, eaque facilis maxime quibusdam esse perferendis nostrum non ut ab eius iste quis laudantium ratione assumenda magni fugiat!
+            {CONTENT[step_num].description}
         </div>
         <div className={styles.blueline}></div>
         <div>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde mollitia deleniti architecto nesciunt laboriosam, nulla dolor totam tempore explicabo dolores natus vero veritatis nam id eligendi quis eaque sunt! Deserunt?
+            {CONTENT[step_num].next_step}
         </div>
     </SidePanel>
 }
