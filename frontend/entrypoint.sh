eda365bf90dfbf4e81afaa286f09cbbd593ad333
guirque <guirque@gmail.com>
2026-03-04 14:01:11 -0300

    build: updating front end infra
    Dependency installation will no longer conflict with volume, as it is now done via the entrypoint script. An environment variable now allows the script to choose between building and starting the Next app or running it in dev mode.

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,6 +2,13 @@
 
 npm install
 
-npm run build
+npm audit fix
 
-exec npm run start
+if [ "$DEV" -eq 0 ]; then
+    npm run build
+    exec npm run start
+    echo "<!> Running in PRODUCTION mode"
+else
+    exec npm run dev
+    echo "<!> Running in DEV mode"
+fi
