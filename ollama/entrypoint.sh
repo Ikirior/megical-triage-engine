e21ac46acabdd37ec1d6672b0827d73d1130ec00
guirque <guirque@gmail.com>
2026-03-04 23:56:43 -0300

    build: adding automatic Ollama image pull

--- ollama/entrypoint.sh
+++ ollama/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
  
 nohup ollama serve &
-ollama pull hf.co/unsloth/medgemma-27b-it-GGUF:Q6_K
-echo "FROM hf.co/unsloth/medgemma-27b-it-GGUF:Q6_K" > Modelfile
-echo "PARAMETER num_ctx 16384" >> Modelfile
-ollama create medgemma-q6-limitado -f Modelfile
-exec ollama run medgemma-q6-limitado
+
+if [ -z "$( ollama ls | grep medgemma-q6-limitado )" ]; then
+    echo "<!> LOADING MODEL"
+    ollama pull hf.co/unsloth/medgemma-27b-it-GGUF:Q6_K
+    echo "FROM hf.co/unsloth/medgemma-27b-it-GGUF:Q6_K" > Modelfile
+    echo "PARAMETER num_ctx 16384" >> Modelfile
+    ollama create medgemma-q6-limitado -f Modelfile
+    ollama rm hf.co/unsloth/medgemma-27b-it-GGUF:Q6_K
+fi
+
+tail -f /dev/null
