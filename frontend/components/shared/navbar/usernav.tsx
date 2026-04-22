d0273730e650a9a2c7842cc7baf93be2ba5ad532
guirque <guirque@gmail.com>
2026-04-22 00:23:57 -0300

    feat: adding basic stylization to user dropdown

--- frontend/components/shared/navbar/usernav.tsx
+++ frontend/components/shared/navbar/usernav.tsx
@@ -3,23 +3,26 @@ import { Home, UserCircle2Icon } from "lucide-react"
 import Link from "next/link"
 import { useState } from "react";
 import styles from '@/components/shared/navbar/navbar.module.css'
-import logoff from "./logoff";
+import logoff from "@/services/navbar/logoff";
 
 export default function UserNav(params: {username: string})
 {
     const [open, setOpen] = useState(false);
     return (
         <button className={"flex gap-2 " + styles.userprofile} onClick={()=>{setOpen(!open)}}> 
-        {params.username} 
-        <UserCircle2Icon/>
-        
-        {
-            open &&
-            <div className={styles.side}>
-                <div onClick={logoff}>Log Off</div>
+            <div className={styles.userinfo}>
+                <div id="username" className={styles.username}>
+                    {params.username} 
+                </div>
+                <UserCircle2Icon/>
             </div>
-        }
-
+            {
+                open &&
+                <div className={styles.side} onPointerLeave={()=>{setOpen(false)}}>
+                    <div>You</div>
+                    <div onClick={logoff}>Log Off</div>
+                </div>
+            }
         </button>
     )
 }
