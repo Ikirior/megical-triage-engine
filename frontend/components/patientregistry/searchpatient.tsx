5d56aecf1b85316971165f7ea590fb456f891f27
guirque <guirque@gmail.com>
2026-04-22 01:39:27 -0300

    fix: fixing error messages not appearing for patient search

--- frontend/components/patientregistry/searchpatient.tsx
+++ frontend/components/patientregistry/searchpatient.tsx
@@ -5,18 +5,22 @@ import PatientGroup from "./patientgroup";
 import GetPatient from "../../services/patientregistry/get_patient";
 import { SearchIcon } from "lucide-react";
 import { patient } from "../../types/Patient";
+import SingleButton from "../shared/singlebutton/singlebutton";
+import ResponseManager from "@/utils/responsemanager";
 
 export default function SearchPatient()
 {
     const [usersState, setUsers] = useState([] as patient[]);
     return <>
-       <form action={async (args) => {
-          const result = await GetPatient(args);
-          setUsers([result.content]);
-       }}>
+       <form>
         <label htmlFor="cpf_search">Search by CPF:</label>
         <input type="text" name="cpf_search" id="cpf_search" placeholder="000.000.000-00" pattern="\d{3}.\d{3}.\d{3}-\d{2}" />
-        <div style={{display: "flex", flexDirection: "row"}}><SearchIcon/><input style={{cursor: "pointer"}} type="submit" value={"Submit"}></input></div>
+        <SingleButton icon={<SearchIcon/>} formAction={async (initialState, args) => {
+            const res = await GetPatient(args);
+            if(res.content)
+               setUsers([res.content])
+            return res;
+        }} text="Search" backgroudColor="transparent" submit/>
       </form>
       <PatientGroup title='' users={usersState} setUsers={setUsers}/>
     </>
